cbb98e95d7387773407a4036f5b0b5ddef22cd43
gaoyishu <gaoyishu91@163.com>
2019-06-06 16:39:50 +0800

    Create meowmeow.java

---
+++ java_class/meowmeow.java
@@ -0,0 +1,14 @@
+# 创建一个简单的类，类的标准格式：class + 类名 + {}
+
+class meowmeow {
+  
+  String name;
+  int age;
+  int weight;
+  
+  # 创建函数用 void
+  void Run(){
+      System.out.println("meowmeow run!");
+  }
+
+}
