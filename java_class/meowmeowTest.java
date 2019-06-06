7cfcc09057ff049037907c82f0f769b29314c028
gaoyishu <gaoyishu91@163.com>
2019-06-06 16:57:26 +0800

    Update meowmeowTest.java

--- java_class/meowmeowTest.java
+++ java_class/meowmeowTest.java
@@ -1,7 +1,9 @@
+# 对meowmeow类进行测试的测试类
 class meowmeowTest {
     public static void main(String[] args) {
         meowmeow meow = new meowmeow();
         d.name = "喵";
         d.age = 10;
+        d.weight = 5;
         d.Run();
 }
